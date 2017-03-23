e424115518d02b25aac3918bdf41fa8af8c4b40e
goparklubanic <nugroho@nujessie.mugeno.org>
2017-03-23 12:59:39 +0700

    filter validiti period

--- www/js/pipas.js
+++ www/js/pipas.js
@@ -9,19 +9,61 @@ $('document').ready(function(){
 	var kl = localStorage.getItem('user_kl');
 		$.getJSON(server+'?opr=pks&mp='+mpid+"&kl="+kl,function(data){
 			$("#dftPaket a").remove();
+			
 			$.each(data,function(index,paket){
+				var olih = validPeriod(paket.start,paket.finish);
+				if(olih == '0' ){
+					var deklik = ">";
+				}else{
+					var deklik = "onClick=gelarSoal("+paket.id+","+paket.soal+")>";
+				}
+				
 				$('#dftPaket').append("<a class='list-group-item ps' "+
-					"onClick=gelarSoal("+paket.id+","+paket.soal+")>"+ 
+					deklik+
 					" Paket No. "+paket.np+
 					"<span class='badge'>"+paket.soal+"</span><br/>"+
-					"<span class='namaguru'>"+paket.guru+"</span></a>"
+					"<div class='timer'><label class='paket-label'>Mulai</label>: "+paket.start+"</div>"+
+					"<div class='timer'><label class='paket-label'>Sampai</label>: "+paket.finish+"</div>"
 					
 				);
 			})
 		});
 		/* **** fungsi getJson **** */
 });
+function validPeriod(st,fh){
+	//new Date(year, month, day, hours, minutes, seconds, milliseconds)
+	var saiki = new Date();
+	var labuh = konvertWaktu(st);
+	var bubar = konvertWaktu(fh);
+	if(saiki > labuh && saiki < bubar){
+		var olih = '1';
+	}else{
+		var olih = '0';
+	}
+	return olih;
+}
 
+function konvertWaktu(wkt){
+	var dj = wkt.split(' ');
+	
+	var dn = dj[0].split('-');
+	var taun = dn[0];
+	var sasi = dn[1];
+	var dina = dn[2];
+	
+	var jm = dj[1].split(':');
+	var j = jm[0];
+	var m = jm[1];
+	var d = jm[2];
+	
+	var tglJam=new Date(taun,sasi,dina,j,m,d,0);
+	return tglJam;
+}
+
+function leadZero(angka){
+	var digit = angka.length;
+	alert(digit);
+}
 function gelarSoal(psid,soal){
 	// buat deretan acak nomor soal dari database
 	var urutan = makeAr(soal);
